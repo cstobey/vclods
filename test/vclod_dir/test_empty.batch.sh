13b931d41cf6be99d5d1e005ecb7c7086aa7a859
Chris Tobey <chris.tobey@covenanteyes.com>
2020-06-11 17:38:50 -0400

    fix bug in the batcher where RESET prints the end even if there was no data

---
+++ test/vclod_dir/test_empty.batch.sh
@@ -0,0 +1,6 @@
+cat << 'EOF'
+#start INSERT INTO a_table (id, a_column, b_column, c_colunm) VALUES 
+#end ON DUPLICATE KEY UPDATE a_column = VALUES(a_column), b_column = VALUES(b_column), c_colunm = VALUES(c_colunm);
+#because RESET was showing the end when the list was empty. 
+#RESET
+EOF
