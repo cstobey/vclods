54ba0ae1f23b8a4905c55ea10c6cce3351fba9e7
Chris Tobey <chris.tobey@covenanteyes.com>
2021-02-04 16:40:27 -0500

    batcher now has custom row separators (RS) configured by the ext-opt.

---
+++ test/vclod_dir/test_newline_batcher.sql.batch-SDRF12.sh
@@ -0,0 +1,12 @@
+cat << 'STMT'
+#batch 2 SDRF12
+#start SELECT * FROM ( SDRF12
+#sep UNION  SDRF12
+#end ) AS t;SDRF12
+ SELECT 'funny
+man' AS name SDRF12
+ SELECT 'silly
+baby' AS name SDRF12
+ SELECT 'pretty
+woman' AS name SDRF12
+STMT
