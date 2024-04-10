9896ca1a2f8aa94c5af710a6e1d2c5664d5c3235
Chris Tobey <chris.tobey@covenanteyes.com>
2024-04-10 16:41:31 -0400

    use a fifo manager instead of xargs; load functions once+add vcurl; add .para (#3)
    details:
    
    switched from using xargs to a custom fifo manager, enabling function propagation.
    broke up vclod_helpers into includes/* system
    added vcurl functions
    extended the test_webserver to cover more test cases
    added .para to mimic the do_dir fifo manager
    moved ksh check from vclod_run_script to .sh where it should be
    got rid of the fifos in vclod_get_semaphore
    merged vclod_get_semaphore and vclod_stdio2log into vclod_run_script
    run_tests.sh cleanup and bugfixes
    made generate_docs handle multiple usages of Variables and added other enhancements
    added vclod_mktemp function and used it everywhere
    updated sort usage for files to include -n for better prepended number handling
    extended .sh doco to include all the includes/* functions... probably need to add info about the collections
    .etl fix a bug with grouping by #generate_unique numbers
    add a kill trap for both fifo managers (do_dir and .para)
    switch from !-z to -n everywhere
    favor printf over echo in most places.
    attempt at .while buffer fix

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,11 @@
 #!/bin/ksh
 
+err() { printf '---FAILED %s\n' "$*" >&2; }
 # setup
 DEBUG_SHOULD_TIME_IT=0 # in case this is being run from the terminal
 LOCAL_DIR="$(dirname "$(readlink -f "$(which "$0")")")/test"
 export DEBUG_SHOULD_TIME_IT LOCAL_DIR
-cd "${LOCAL_DIR}" || { echo >&2 "Could not cd into the test dir... something is REALLY wrong" ; exit 1; }
+cd "${LOCAL_DIR}" || { err "Could not cd into the test dir... something is REALLY wrong" ; exit 1; }
 mkdir -p ./logs/ ./files/ ./tmp_files/
 rm -f logs/* files/* tmp_files/*
 numb_lines="$(cat expected_logs/* | wc -l)"
@@ -30,49 +31,48 @@ ret=$? # run the primary test
 rm -f ./vclod_dir/local_config_override/test_symlink.sh
 
 echo 'echo $VCLOD_JOBS' | O_VCLOD_JOBS=200 CONFIG_FILE="${LOCAL_DIR}/config" ../vclod_do_dir test_cli_override_env.sh
-ret="$((ret + $?))" # test env override and stdin streams. 
+ret="$((ret + $?))" # test env override and stdin streams.
 
 # tests off the previous run
 
 # syslog
-comm -23 <(cat logs/*.log | sed -r 's/^[^[]* ([0-9]+ [[])/\1/' | sort) <(sudo tail /var/log/messages -n"$((numb_lines*2))" | sed -r 's/^([^ \t]+[ \t]+){5}//' | sort) | grep . && echo "FAILED syslog data and pid mismatch" >&2
+comm -23 <(cut -d' ' -f3- logs/*.log | sort) <(sudo grep -o "$(whoami): .*$" /var/log/messages | tail -n"$((numb_lines*2))" | cut -d' ' -f2- | sort) | grep . && err "syslog data and pid mismatch"
 # log file check
-find expected_logs/*.expected logs/*.log | sed -r 's/(^expected_|.expected$)//g;s/[.][0-9]+[.]log$//' | sort | uniq -c | grep -Ev '^ +2 ' && echo >&2 "FAILED to render the right log files"
+find expected_logs/*.expected logs/*.log | sed -r 's/(^expected_|.expected$)//g;s/[.][0-9]+[.]log$//' | sort | uniq -c | grep -Ev '^ +2 ' && err "to render the right log files"
 # log file content check : 2 methods, I think I like the second way
 #find expected_logs/*.expected | sed -r 's#^(expected)_(.+).expected$#diff -w <(sort \1_\2.\1) <(sed -r '"'"'s/^[^[]+[[]/[/;s/[.][0-9]+[.]log/..log/'"'"' \2.[0-9]*.log | sort) || echo >\&2 "FAILED content \2 mismatch"#' | . /dev/stdin
-for f in expected_logs/*.expected ; do diff -w <(sort "$f") <(sed -r 's/^[^[]+[[]/[/;s/[.][0-9]+[.]log/..log/' "logs/$(basename "${f%expected}")"[0-9]*.log | sort) || echo "FAILED $f content mismatch" >&2 ; done
+for f in expected_logs/*.expected ; do diff -w <(sort "$f") <(sed -r 's/^[^[]+[[]/[/;s/[.][0-9]+[.]log/..log/' "logs/$(basename "${f%expected}")"[0-9]*.log | sort) || err "$f content mismatch" ; done
 # test the outfiles... cant test the append, since they put date parts on the ends of the filenames... need to think more about that.
 for f in files/* ; do cp "$f" "tmp_files/$(basename "${f/-"$(date +%F)"/}")" ; done
 for f in $(ls -1 logs/*.g-* | grep -E '[.]g-[^.]*$') ; do cp "$f" "tmp_files/$(basename "${f}" | sed -r 's/\.[0-9]+\.log//')" ; done
-diff -wr expected_files tmp_files || echo "FAILED to render the right output files with the right content" >&2
+diff -wr expected_files tmp_files || err "to render the right output files with the right content"
 
 # ensure that the post process log2sql process works in all ways.
-cat << 'EOF' | CONFIG_FILE="${LOCAL_DIR}/config" ../vclod_do_dir test_pp_logger.sh || echo "FAILED to export log2sql" >&2
-export SRC=LOG_SQL_
-diff -w <(cat << 'STMT' | vclod_operation get_sql.sql | sed 's/\t/ /g' | sort
+cat << 'EOF' | CONFIG_FILE="${LOCAL_DIR}/config" O_SRC=LOG_SQL_ ../vclod_do_dir test_pp_logger.out.sh || err "to export log2sql"
+diff -w <(cat << 'STMT' | vclod_operation get_sql.sql
 SELECT
-  SUBSTRING_INDEX(log_file, '/', -1) AS base_file, 
-  ROW_NUMBER() OVER (PARTITION BY log_file, pid ORDER BY script_log_id) AS line_number, 
-  happened_at, pid, tag, message, IFNULL(value, '') AS value 
-FROM script_log;
+  CONCAT('logs/', SUBSTRING_INDEX(log_file, '/', -1), '.', pid, '.log:',
+    ROW_NUMBER() OVER (PARTITION BY log_file, pid ORDER BY script_log_id), ':',
+    CONCAT_WS(' ', happened_at, pid, tag, message, value))
+FROM script_log ORDER BY 1;
 STMT
-) <(find logs/* ! -empty -xtype f -regextype posix-egrep -regex '.*[.]log$' | xargs grep ^ -n | sed -r 's#^logs/(.*)[.][0-9]+[.]log:([0-9]+):#\1 \2 #' | sort)
+) <(grep ^ -n logs/*.log | sort)
 EOF
 
 # untested extentions
-comm -13 <(find vclod_dir/ | grep -Eo '[.][a-z]+' | sort -u | sed 's/.//') <(find ../extensions/* | grep -Eo '[^/]+$') | sed 's/^/UNTESTED EXTENSION /' >&2
+comm -13 <(find vclod_dir/ | grep -Eo '[.][a-z]+' | sort -u) <(find ../extensions/* | sed -r 's#^.*/([^/]+)$#.\1#') | paste -sd',' | sed 's/^/---UNTESTED EXTENSIONS/;s/[.]/ /g' >&2
 
 # extra tests
 
 # test single file
-CONFIG_FILE="${LOCAL_DIR}/config" ../vclod_do_dir test_single_file.diff.sh || echo "FAILED single file should render without the directory $?"
+CONFIG_FILE="${LOCAL_DIR}/config" ../vclod_do_dir test_single_file.diff.sh || err "single file should render without the directory $?"
 # stdin
-cat << 'EOF' | CONFIG_FILE="${LOCAL_DIR}/config" ../vclod_do_dir test_stdin.diff-test_single_file.sh || echo "FAILED virtual single file should render without the directory $?"
+cat << 'EOF' | CONFIG_FILE="${LOCAL_DIR}/config" ../vclod_do_dir test_stdin.diff-test_single_file.sh || err "virtual single file should render without the directory $?"
 echo A single file needs to work by itself
 EOF
 # sh_only
-CONFIG_FILE="${LOCAL_DIR}/sh_only/config" ../vclod_do_dir ./sh_only/ || echo "FAILED mysql should not be required when not used $?"
+CONFIG_FILE="${LOCAL_DIR}/sh_only/config" ../vclod_do_dir ./sh_only/ || err "mysql should not be required when not used $?"
 
-diff <(find $VCLOD_ERR_DIR $LOCAL_DIR -type p) <(printf "") || echo "FAILED to clean up fifo files: find $VCLOD_ERR_DIR $LOCAL_DIR -type p -delete>&2" >&2
+diff <(find $VCLOD_ERR_DIR $LOCAL_DIR -type p) <(printf "") || err "to clean up fifo files: find $VCLOD_ERR_DIR $LOCAL_DIR -type p -delete>&2"
 
 exit $ret
---
+++ test/vclod_dir/test_curl.jq-jq_prog.sh
@@ -0,0 +1,33 @@
+sleep 1
+#CURL_EXT_LOOP_CNT=1
+vcurl_run "http://localhost:9000/page_1.json"
+
+from_total()
+{
+  [ "$VCURL_LOOP_COUNTER" -lt "$(JQ_EXT_PROG='.total' vclod_operation get_total.jq <"$VCURL_LAST_OUTPUT")" ] && echo "http://localhost:9000/cgi-bin/query?cnt=$((VCURL_LOOP_COUNTER+1))&funny=man"
+}
+vcurl_while from_total #"http://localhost:9000/cgi-bin/query?cnt=1&funny=man"
+
+next_page()
+{
+  JQ_EXT_OPT='-r' JQ_EXT_PROG='.next' vclod_operation get_next_url.jq <"$VCURL_LAST_OUTPUT" 2>/dev/null | sed 's/^null$//'
+}
+vcurl_while next_page "http://localhost:9000/next_page_1.json"
+
+cat << 'EOF' | vcurl_multi_run
+http://localhost:9000/page_1.json
+http://localhost:9000/page_2.json
+EOF
+
+echo 'test=post&silly=woman' | vcurl_run "http://localhost:9000/cgi-bin/query" -d @-
+vcurl_run "http://localhost:9000/cgi-bin/query" -d 'test=argument' -d'silly=woman'
+#echo >&2 "files in script: $VCURL_LAST_FULL_OUT, $VCURL_LAST_ERROR, $VCURL_LAST_HEADER, $VCURL_LAST_OUTPUT"
+#echo >&2 "[INFO] $(grep -n ^$ "$VCURL_LAST_FULL_OUT")"
+#echo >&2 [INFO] all
+#sed 's/^/[INFO] /' $VCURL_LAST_FULL_OUT >&2
+#echo >&2 [INFO] headers
+#cat $VCURL_LAST_HEADER >&2
+#echo >&2 [INFO] errors
+#cat $VCURL_LAST_ERROR >&2
+#echo >&2 [INFO] output
+#cat $VCURL_LAST_OUTPUT >&2
